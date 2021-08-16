0988b404b07e60ff01b0cbcde2139e3a25b83195
Batoolayyad <batoolayyad1996@gmail.com>
2021-08-16 23:46:44 +0300

    add local storage for lab32

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -10,15 +10,11 @@ function ToDo(props) {
   const [state, setState] = useState([])
 
   useEffect(()=>{
-    let list = [
-      { _id: 1, complete: false, text: 'Clean the Kitchen', difficulty: 3, assignee: 'Person A' },
-      { _id: 2, complete: false, text: 'Do the Laundry', difficulty: 2, assignee: 'Person A' },
-      { _id: 3, complete: false, text: 'Walk the Dog', difficulty: 4, assignee: 'Person B' },
-      { _id: 4, complete: true, text: 'Do Homework', difficulty: 3, assignee: 'Person C' },
-      { _id: 5, complete: false, text: 'Take a Nap', difficulty: 1, assignee: 'Person B' },
-    ];
-    
-    setState(list);
+    let localSettings = localStorage.getItem('settings') === true;
+    if(localSettings){
+      localSettings = JSON.parse(localStorage.getItem('settings'))
+      settingContext.setSettings(localSettings.items,localSettings.view);
+    }
   },[])
   
 
